9ba8782af7d3ef38949b37ac7675cc338e5838b4
Shaurya Jha <shauryajha35@gmail.com>
2024-10-11 19:43:19 +0530

    Add project loader to the right position

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -21,59 +21,59 @@ const Page = () => {
 
   return (
     <div className="h-[calc(100%-100px)] text-gray-custom font-fira-code flex ">
-      {loading ? (
-        <div className="w-full h-full flex items-center justify-center">
-          <Loader />
-        </div>
-      ) : (
-        <>
-          <div className="border-r border-white w-[391px] hidden lg:block">
-            <div className="flex flex-row text-white-custom border-b border-white w-full h-[35px] justify-start items-center text-[14px] cursor-pointer">
-              <div className="pl-2 flex justify-center items-center">
-                <DownIcon />
-              </div>
-              <div className="pl-2">projects</div>
-            </div>
-            <div className="flex flex-col px-[25px] py-[10px] border-b border-white">
-              <div className="flex flex-row py-2 hover:text-white-custom cursor-pointer">
-                <div>
-                  <Right />
-                </div>
-                <div className="pl-1">Nextjs</div>
-              </div>
-              <div className="flex flex-row py-2 hover:text-white-custom cursor-pointer">
-                <div>
-                  <Right />
-                </div>
-                <div className="pl-1">Reactjs</div>
-              </div>
-              <div className="flex flex-row py-2 hover:text-white-custom cursor-pointer">
-                <div>
-                  <Right />
-                </div>
-                <div className="pl-1">Tailwind</div>
-              </div>
-              <div className="flex flex-row py-2 hover:text-white-custom cursor-pointer">
-                <div>
-                  <Right />
-                </div>
-                <div className="pl-1">Nodejs</div>
-              </div>
-              <div className="flex flex-row py-2 hover:text-white-custom cursor-pointer">
-                <div>
-                  <Right />
-                </div>
-                <div className="pl-1">Expressjs</div>
-              </div>
-            </div>
+      <div className="border-r border-white w-[391px] hidden lg:block">
+        <div className="flex flex-row text-white-custom border-b border-white w-full h-[35px] justify-start items-center text-[14px] cursor-pointer">
+          <div className="pl-2 flex justify-center items-center">
+            <DownIcon />
           </div>
-          <div className="w-full h-full flex flex-col">
-            <div className="hidden lg:flex flex-row text-white-custom border-b border-white w-full h-[35px] justify-start items-center text-[14px] cursor-pointer">
-              <div className="pl-4">page.tsx</div>
-              <div className="pl-4 pr-2 border-r border-white h-full flex justify-center items-center">
-                <CrossIcon />
-              </div>
+          <div className="pl-2">projects</div>
+        </div>
+        <div className="flex flex-col px-[25px] py-[10px] border-b border-white">
+          <div className="flex flex-row py-2 hover:text-white-custom cursor-pointer">
+            <div>
+              <Right />
             </div>
+            <div className="pl-1">Nextjs</div>
+          </div>
+          <div className="flex flex-row py-2 hover:text-white-custom cursor-pointer">
+            <div>
+              <Right />
+            </div>
+            <div className="pl-1">Reactjs</div>
+          </div>
+          <div className="flex flex-row py-2 hover:text-white-custom cursor-pointer">
+            <div>
+              <Right />
+            </div>
+            <div className="pl-1">Tailwind</div>
+          </div>
+          <div className="flex flex-row py-2 hover:text-white-custom cursor-pointer">
+            <div>
+              <Right />
+            </div>
+            <div className="pl-1">Nodejs</div>
+          </div>
+          <div className="flex flex-row py-2 hover:text-white-custom cursor-pointer">
+            <div>
+              <Right />
+            </div>
+            <div className="pl-1">Expressjs</div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full h-full flex flex-col">
+        <div className="hidden lg:flex flex-row text-white-custom border-b border-white w-full h-[35px] justify-start items-center text-[14px] cursor-pointer">
+          <div className="pl-4">page.tsx</div>
+          <div className="pl-4 pr-2 border-r border-white h-full flex justify-center items-center">
+            <CrossIcon />
+          </div>
+        </div>
+        {loading ? (
+          <div className="w-full h-full flex items-center justify-center">
+            <Loader />
+          </div>
+        ) : (
+          <>
             <div className="flex-1 overflow-y-auto p-10 flex flex-wrap justify-evenly items-start gap-8 scrollable-div">
               <ProjectsPanel
                 projectNumber={1}
@@ -220,9 +220,9 @@ const Page = () => {
                 githubLink="https://github.com/shaurya35/Interactive-Nextjs-Calendar"
               />
             </div>
-          </div>
-        </>
-      )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
