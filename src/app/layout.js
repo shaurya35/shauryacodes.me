a04d62600080f9e8b83a9a57d34fdbb79a3829c2
Shaurya <shauryajha35@gmail.com>
2025-11-20 23:30:05 +0530

    Update SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import GithubCorner from "../components/GithubCorner";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,13 +25,19 @@ export default function RootLayout({ children }) {
 
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
+        {/* https://app.ahrefs.com/ analytics  */}
+        <script
+          src="https://analytics.ahrefs.com/analytics.js"
+          data-key="I9918vQBJeWMi++51KD3wQ"
+          async
+        ></script>
       </head>
       <body className={inter.className}>
         <div className="bg-black-100 w-screen h-screen overflow-hidden flex items-center justify-center">
           <div className="bg-black-200 m-[15px] lg:m-[30px] rounded-lg border border-white flex flex-col w-[calc(100%-30px)] lg:w-[calc(100%-60px)] h-[calc(100%-30px)] lg:h-[calc(100%-60px)]">
             <GithubCorner />
             <Header />
-            <Analytics/>
+            <Analytics />
             {children}
             <Footer />
           </div>
